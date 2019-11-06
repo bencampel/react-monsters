39250446d6d2c870cd8b8e889b3bed2747d8c61f
Diego Aguirre <diegoandres25717@gmail.com>
2019-11-06 00:40:08 -0300

    Updates

---
+++ precache-manifest.342002ac8af0dd6a9b86691ee9f0dfd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76a880734c6c90bbda357f49fc85693",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "41e0c4b0ad4beea4ae0d",
+    "url": "/react-monsters/static/css/main.02ad09a9.chunk.css"
+  },
+  {
+    "revision": "02f1f6cb492a7ab9fe4a",
+    "url": "/react-monsters/static/js/2.e2aab318.chunk.js"
+  },
+  {
+    "revision": "41e0c4b0ad4beea4ae0d",
+    "url": "/react-monsters/static/js/main.6bb68203.chunk.js"
+  },
+  {
+    "revision": "a7ac8baf17b6a9e4e2f9",
+    "url": "/react-monsters/static/js/runtime-main.ba1fc5cb.js"
+  }
+]);
